931e012f527d206197f46247580404c4269afd83
MicrQ <mail.micrq@proton.me>
2023-11-30 20:13:32 +0300

    message edited

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 if (__name__ == "__main__"):
-    import sys
-    argc = len(sys.argv)
-    print("{:d} arguments".format(argc - 1))
+    from sys import argv
+    argc = len(argv)
+    if (argc - 1 == 0):
+        message = "arguments."
+    elif (argc - 1 == 1):
+        message = "argument:"
+    else:
+        message = "arguments:"
+    print("{:d} {}".format(argc - 1, message))
     for i in range(1, argc):
-        print("{:d}: {}".format(i, sys.argv[i]))
+        print("{:d}: {}".format(i, argv[i]))
