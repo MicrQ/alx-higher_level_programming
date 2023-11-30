0111b67a30af42ef6f197f7292d7ec300bdbea74
MicrQ <mail.micrq@proton.me>
2023-11-30 20:04:51 +0300

    removed alias

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if (__name__ == "__main__"):
-    import sys as s
-    argc = len(s.argv)
+    import sys
+    argc = len(sys.argv)
     print("{:d} arguments".format(argc - 1))
     for i in range(1, argc):
-        print("{:d}: {}".format(i, s.argv[i]))
+        print("{:d}: {}".format(i, sys.argv[i]))
