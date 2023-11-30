1626a3be2a29a892d684b11df666b9fdcdbc8f03
MicrQ <mail.micrq@proton.me>
2023-11-30 21:55:05 +0300

    cutom calculator in python

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if (__name__ == "__main__"):
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
+    argc = len(argv)
+    if (argc != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        a, b = int(argv[1]), int(argv[3])
+        if (argv[2] not in '*+/-'):
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+        elif (argv[2] == "+"):
+            print("{:d} {} {:d} = {:d}".format(a, argv[2], b, a + b))
+        elif (argv[2] == "-"):
+            print("{:d} {} {:d} = {:d}".format(a, argv[2], b, a - b))
+        elif (argv[2] == "*"):
+            print("{:d} {} {:d} = {:d}".format(a, argv[2], b, a * b))
+        else:
+            print("{:d} {} {:d} = {}".format(a, argv[2], b, a / b))
