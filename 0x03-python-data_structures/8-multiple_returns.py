833ea6d5337edaf568d39a66c4c18028de380c5d
MicrQ <mail.micrq@proton.me>
2023-12-03 22:07:45 +0300

    empty string as 'None'

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is "":
-        char0 = None
+    if len(sentence) is 0:
+        char0 = "None"
     else:
         char0 = sentence[0]
     return (len(sentence), char0)
