1e62aeb441455eed98b44665e10b149f990b5a76
MicrQ <mail.micrq@proton.me>
2023-12-03 22:28:55 +0300

    None in ''

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-
-    char0 = sentence[0] if length > 0 else None
+    char0 = sentence[0] if length > 0 else "None"
     tup = (length, char0)
-    return(tup)
+    return (tup)
