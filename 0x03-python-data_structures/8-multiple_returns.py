537125ab4700090096882837b39aed3437d4ef91
MicrQ <mail.micrq@proton.me>
2023-12-03 22:17:13 +0300

    multiple returns handled for empty string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 def multiple_returns(sentence):
     length = len(sentence)
 
-    char0 = sentence[0] if length > 0 else "None"
+    char0 = sentence[0] if length > 0 else None
     tup = (length, char0)
     return(tup)
