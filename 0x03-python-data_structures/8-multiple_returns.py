840126e4cdb489f9635ea7b212f9291db3ea4e26
MicrQ <mail.micrq@proton.me>
2023-12-03 22:15:37 +0300

    multi return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    lgz = len(sentence)
-    char0 = sentence[0] if lgz > 0 else "None"
-    tup = lgz, char0
+    length = len(sentence)
+
+    char0 = sentence[0] if length > 0 else "None"
+    tup = (length, char0)
     return(tup)
