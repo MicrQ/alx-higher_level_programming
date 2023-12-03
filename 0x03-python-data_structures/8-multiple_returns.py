98e58fa0d32916ae91985a073421e86a2f57a09f
MicrQ <mail.micrq@proton.me>
2023-12-03 22:02:46 +0300

    multiple return is possible with tuple modified

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is "":
-        char0 = ""
-    else:
-        char0 = sentence[0]
-    return (len(sentence), char0)
+    return (len(sentence), sentence[0])
