281ec48cf4ad8708e689aaa448113d3273fd8642
MicrQ <mail.micrq@proton.me>
2023-12-06 17:30:21 +0300

    a function that compares the value of a dictionary keys and returns the key that have the maximum value

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return
+    max_score = list(a_dictionary)[0]
+    max_value = a_dictionary[max_score]
+    for i in a_dictionary:
+        if (a_dictionary[i] > max_value):
+            max_score = i
+    return (max_score)
+        
