378e6818df733ea4ca3336820ff4d01b6b4eba9f
MicrQ <mail.micrq@proton.me>
2023-12-06 18:21:04 +0300

    a function that calculates the weigthed average of a list of tuples

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    sum_ = 0
+    div = 0
+    for tup in my_list:
+        sum_ = sum_ + (tup[0] * tup[1])
+        div += tup[1]
+    return (sum_ / div)
