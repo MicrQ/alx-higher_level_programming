af02c04cb1e873c9757fe575087ffd16015146d8
MicrQ <mail.micrq@proton.me>
2023-12-20 09:43:04 +0300

    a linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -10,33 +10,33 @@ class Node:
             data(int): data for the node
             next_node(Node): the next node
         """
-        data(data)
-        next_node(next_node)
-
+        self.data = data
+        self.next_node = next_node
 
     @property
     def data(self):
         """Getter"""
         return (self.__data)
-    
+
     @data.setter
     def data(self, value):
         """data setter"""
         if type(value) != int:
             raise TypeError("data must be an integer")
         self.__data = value
-    
+
     @property
     def next_node(self):
         """next_node getter"""
         return (self.__next_node)
-    
+
     @next_node.setter
     def next_node(self, value):
         """setter of next_node"""
-        if type(value) != Node and value != None:
+        if type(value) != Node and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
+
 
 """A singly linked list class"""
 
@@ -45,8 +45,34 @@ class SinglyLinkedList:
     """defines a node"""
     def __init__(self):
         """"""
-        pass
+        self.__head = None
 
     def sorted_insert(self, value):
         """inserts a new node"""
-        
+        new = Node(value)
+        ptr = self.__head
+        hold = None
+
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+
+        elif value < self.__head.data:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            while (ptr.next_node is not None and
+                    ptr.next_node.data < value):
+                ptr = ptr.next_node
+            new.next_node = ptr.next_node
+            ptr.next_node = new
+
+    def __str__(self):
+        """str"""
+        sData = []
+        hold = self.__head
+
+        while hold:
+            sData.append(str(hold.data))
+            hold = hold.next_node
+        return ('\n'.join(sData))
