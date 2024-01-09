3d42cc581e81f1883e24b9eface68847a4acce75
MicrQ <mail.micrq@proton.me>
2024-01-09 03:22:41 +0300

    3: adding attribute if possible

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,6 +3,16 @@
 
 
 def add_attribute(obj, name, value):
+    """adds a new attribute if possible
+
+    Args:
+        obj (object): to add new attribute
+        name (any): key
+        value (any): value
+
+    Raises:
+        TypeError: can't add new attribute
+    """
     if hasattr(obj, '__dict__'):
         obj.__dict__[name] = value
     else:
