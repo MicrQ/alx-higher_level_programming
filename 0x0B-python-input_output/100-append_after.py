b966fe45ddbe0b1c53dde5a2afc6254c38681e1f
MicrQ <mail.micrq@proton.me>
2024-01-10 13:43:26 +0300

    utf-8 : appending after a specific word found in the line

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,12 +5,12 @@
 def append_after(filename="", search_string="", new_string=""):
     """appends after searching"""
     to_write = ''
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, "r") as f:
         line = f.readline()
         while(line != ''):
             to_write += line
             if search_string in line:
                 to_write += new_string
             line = f.readline()
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, "w") as f:
         f.write(to_write)
