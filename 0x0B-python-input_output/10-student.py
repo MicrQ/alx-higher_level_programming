4ff2f8dcc7400122edecae7dcb6b14dd88fb9ebf
MicrQ <mail.micrq@proton.me>
2024-01-10 05:57:50 +0300

    task to student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""student class"""
+
+
+class Student:
+    """represents student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """to retrive a dictiionary representation"""
+        if attrs is None:
+            return self.__dict__
+
+        new_dict = {}
+        for item in attrs:
+            try:
+                new_dict[item] = self.__dict__[item]
+            except Exception:
+                pass
+        return new_dict
