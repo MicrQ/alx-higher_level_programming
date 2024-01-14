74cea149194ac12791e7d35f6a1d977e4c1ccd95
MicrQ <mail.micrq@proton.me>
2024-01-15 00:47:58 +0300

    rectangle pycodestyle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -92,7 +92,7 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {:d}/{:d} - {:d}/{:d}".format(
             self.id, self.__x, self.__y, self.__width, self.__height
         )
-    
+
     def update(self, *args):
         """updates given arguments to attributes
         args:
