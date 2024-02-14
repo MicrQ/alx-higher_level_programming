83cb680e6166d1f81a716eb4939fc3643f144659
MicrQ <mail.micrq@proton.me>
2024-02-14 11:37:40 +0300

    a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending) of 'temperatures' table.

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending) of 'temperatures' table.
-SELECT `city`, AVG(`value`) AS `avr_temp`
-FROM `temperatures` GROUP BY `city` ORDER BY `avr_temp` DESC;
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC;
