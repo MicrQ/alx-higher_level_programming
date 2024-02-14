42add011a2a43aeb69b9204c1ab66da9d47f24ad
MicrQ <mail.micrq@proton.me>
2024-02-14 09:28:12 +0300

    MODIFIED: a script that creates a table second_table(id int, name varchar(256) and score int). and inserts mutiple values

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- a script that creates a table second_table(id int, name varchar(256) and score int). and inserts mutiple values
 CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) (2, "Alex", 3);
-INSERT INTO `second_table` (`id`, `name`, `score`) (3, "Bob", 13);
-INSERT INTO `second_table` (`id`, `name`, `score`) (4, "George", 8);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 13);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
