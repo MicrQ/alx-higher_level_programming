5c51f4a161bd9ab0155e597241363c27638c53ca
MicrQ <mail.micrq@proton.me>
2024-03-11 18:59:29 +0300

    factorial of n

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (n === 0 || n === 1) { return 1; }
+  return n * factorial(n - 1);
+}
+const n = parseInt(process.argv[2]);
+if (isNaN(n)) {
+  console.log(1);
+} else {
+  console.log(factorial(n));
+}
