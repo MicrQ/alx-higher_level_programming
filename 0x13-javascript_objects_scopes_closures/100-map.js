9f9560020329975873bb3f6a139b1dc10ff3b8f8
MicrQ <mail.micrq@proton.me>
2024-03-12 17:27:13 +0300

    the function in map is modified

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const list = require('./100-main').list;
 
-const newArray = list.map((n, i) => n * i);
+const newArray = list.map(function (n, i) {
+  return n * i;
+});
 
 console.log(list);
 console.log(newArray);
