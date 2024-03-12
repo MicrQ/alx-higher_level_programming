cbc5ccc82c21bdaf41a91e638a36fae6e59074f9
MicrQ <mail.micrq@proton.me>
2024-03-12 16:19:28 +0300

    a square class that prints the square with the given character

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const ParentSquare = require('./5-square');
+
+class Square extends ParentSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += c;
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Square;
