04397f06dc7a3283b0f0bc1c070141c0c0f5e73d
MicrQ <mail.micrq@proton.me>
2024-03-16 19:37:13 +0300

    script that prints the first    State object from the database hbtn_0e_6_usa    using sqlalchemy module

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" a script that prints the first
+    State object from the database hbtn_0e_6_usa
+    using sqlalchemy module
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import State, Base
+""" mandatory + necessary modules """
+
+engine = create_engine(
+    "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]
+    ), pool_pre_ping=True)
+
+Base.metadata.create_all(engine)
+session = Session(engine)
+
+result = session.query(State).first()
+
+if result:
+    print("{}: {}".format(result.id, results.name))
+else:
+    print('Nothing')
+
+session.close()
