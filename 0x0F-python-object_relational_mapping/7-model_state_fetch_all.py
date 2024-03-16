b12be516433af9a51189458ce04bbe72c3947abd
MicrQ <mail.micrq@proton.me>
2024-03-16 19:11:10 +0300

    if __name__: a script that lists all State objects from the database hbtn_0e_6_usa    using SQLAlchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,22 +2,22 @@
 """ a script that lists all State objects from the database hbtn_0e_6_usa
     using SQLAlchemy
 """
+if __name__ == '__main__':
+    from model_state import State, Base
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sys import argv
+    """ Importing the Base declarative and state class """
 
-from model_state import State, Base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from sys import argv
-""" Importing the Base declarative and state class """
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-engine = create_engine(
-    "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
 
-Base.metadata.create_all(engine)
-session = Session(engine)
+    results = session.query(State).order_by(State.id).all()
 
-results = session.query(State).order_by(State.id).all()
-
-for row in results:
-    print("{}: {}".format(row.id, row.name))
-session.close()
+    for state in results:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
