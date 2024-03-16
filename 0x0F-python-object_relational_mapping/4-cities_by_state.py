d2627614d15f1f5857b5f4f15041a298e73a9f00
MicrQ <mail.micrq@proton.me>
2024-03-16 16:43:22 +0300

    MODIFIED: a script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,6 +9,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM `cities` ORDER BY `id`")
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities LEFT JOIN states \
+    ON cities.state_id = states.id ORDER BY cities.id")
 
     [print(row) for row in cur.fetchall()]
