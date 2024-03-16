4a7e9ac351e325a75201335be4eb58d7d7972575
MicrQ <mail.micrq@proton.me>
2024-03-16 10:06:56 +0300

    if __main__: README file && a python script that lists all states from the database hbtn_0e_0_usa using MySQLdb module

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,11 +4,11 @@
 from sys import argv
 import MySQLdb
 """ Importing the necessary modules """
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
 
-db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
 
-cur = db.cursor()
-
-cur.execute('SELECT * FROM `states` ORDER BY `id` ASC')
-for row in cur.fetchall():
-    print(row)
+    cur.execute('SELECT * FROM `states` ORDER BY `id` ASC')
+    for row in cur.fetchall():
+        print(row)
