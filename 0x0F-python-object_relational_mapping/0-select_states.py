8f3c8816c93d4502e7f36b74009479c5d3070857
MicrQ <mail.micrq@proton.me>
2024-03-16 10:02:36 +0300

    README file && a python script that lists all states from the database hbtn_0e_0_usa using MySQLdb module

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" a script that lists all states from the database hbtn_0e_0_usa """
+
+from sys import argv
+import MySQLdb
+""" Importing the necessary modules """
+
+db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+
+cur = db.cursor()
+
+cur.execute('SELECT * FROM `states`')
+for row in cur.fetchall():
+    print(row)
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,4 @@
+# Python - Object-relational mapping (ORM)
+
+This project is part of <b>ALXSE Program</b>, It focuses on python database connection with different fromworks and libraries.
+
