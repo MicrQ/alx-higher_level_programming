3a7f09c2d5496e4c2062df5edbb77bfe408031b7
MicrQ <mail.micrq@proton.me>
2024-03-16 10:05:38 +0300

    ORDER BY: README file && a python script that lists all states from the database hbtn_0e_0_usa using MySQLdb module

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,6 +9,6 @@ db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
 
 cur = db.cursor()
 
-cur.execute('SELECT * FROM `states`')
+cur.execute('SELECT * FROM `states` ORDER BY `id` ASC')
 for row in cur.fetchall():
     print(row)
