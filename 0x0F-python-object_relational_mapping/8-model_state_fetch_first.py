ec0c3aba49924b2fe0bf3352230090157d5c69ff
MicrQ <mail.micrq@proton.me>
2024-03-16 19:38:27 +0300

    Modified

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,25 +3,27 @@
     State object from the database hbtn_0e_6_usa
     using sqlalchemy module
 """
-from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from model_state import State, Base
-""" mandatory + necessary modules """
 
-engine = create_engine(
-    "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3]
-    ), pool_pre_ping=True)
+if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from model_state import State, Base
+    """ mandatory + necessary modules """
 
-Base.metadata.create_all(engine)
-session = Session(engine)
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            argv[1], argv[2], argv[3]
+        ), pool_pre_ping=True)
 
-result = session.query(State).first()
+    Base.metadata.create_all(engine)
+    session = Session(engine)
 
-if result:
-    print("{}: {}".format(result.id, results.name))
-else:
-    print('Nothing')
+    result = session.query(State).first()
 
-session.close()
+    if result:
+        print("{}: {}".format(result.id, results.name))
+    else:
+        print('Nothing')
+
+    session.close()
