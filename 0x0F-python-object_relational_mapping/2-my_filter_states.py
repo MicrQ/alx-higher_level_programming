f76ebd62117292e1329675296abfff0383b6778d
MicrQ <mail.micrq@proton.me>
2024-03-16 11:32:11 +0300

    modified

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,16 +2,16 @@
 """  a script that takes in an argument and displays all values
     in the states table of hbtn_0e_0_usa where name matches the argument.
 """
+import MySQLdb
+from sys import argv as av
 
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv as av
     """ necessary modules """
 
     db = MySQLdb.connect(user=av[1], passwd=av[2], db=av[3])
     cur = db.cursor()
 
-    cur.execute("""SELECT * FROM `states`
-    WHERE `name` = '{}' ORDER BY id ASC""".format(av[4]))
+    cur.execute("SELECT * FROM `states` \
+    WHERE `name` = '{}' ORDER BY id ASC".format(av[4]))
 
     [print(row) for row in cur.fetchall()]
