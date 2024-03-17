6bf7f15681786acce8c24272994e537b4e0a2016
MicrQ <mail.micrq@proton.me>
2024-03-17 11:24:29 +0300

    class definition of City and a that prints all City objects from the database hbtn_0e_14_usa

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" a that prints all City objects from the database hbtn_0e_14_usa """
+
+if __name__ == '__main__':
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from model_state import Base, State
+    from model_city import City
+    from sys import argv
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    results = session.query(State, City)\
+        .filter(State.id == City.state_id).all()
+
+    for state, city in results:
+        print('{}: ({}) {}'.format(state.name, city.id, city.name))
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" class definition for City class """
+
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, unique=True,
+                nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
