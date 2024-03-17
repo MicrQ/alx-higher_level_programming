6dc6c3cb36065d5b98cf15268a54ad7deca0eca8
MicrQ <mail.micrq@proton.me>
2024-03-17 08:37:31 +0300

    documetation modified

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ a python file that contains the class definition of
-    a State and an instance Base = declarative_base().
+    a State and Base = declarative_base().
 """
 
 from sqlalchemy import create_engine, Column, Integer, String
