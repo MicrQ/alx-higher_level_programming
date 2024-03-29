2108c2ba08bfe88bf7c11d73788b276bb4784d8f
MicrQ <mail.micrq@proton.me>
2024-03-30 01:18:40 +0300

    MODIFIED: a Bash script that causes the server to respond You got me!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that causes the server to respond 'You got me!'
-curl -sd "data=You got me!" -X POST 0.0.0.0:5000/catch_me
+curl -sX POST -d "data=You got me!" 0.0.0.0:5000/catch_me
