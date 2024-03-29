b916c60cb42919d1507fa6e4928c6f1bce71b1a3
MicrQ <mail.micrq@proton.me>
2024-03-29 22:55:47 +0300

    MODIFIED: a  Bash script that displays the size of the body of the response.

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # a  Bash script that displays the size of the body of the response.
-curl -sI GET $1 | grep Content-Length | cut -d ' ' -f2
+curl -sI $1 | grep Content-Length | cut -d ' ' -f2
