d12c52a4177276c9d491a9da6e290640624c6327
MicrQ <mail.micrq@proton.me>
2024-03-29 23:32:28 +0300

    MODIFIED1: a script that displays only body of 200 status code after a GET request.

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script that displays only body of 200 status code after a GET request.
-if [ $(curl -sI GET $1 | head -1 | cut -d ' ' -f2) == 200 ]; then curl -sL $1; fi
+if [ $(curl -sLo /dev/null -w "%{http_code}" "$1") == '200' ]; then curl -sL $1; fi
