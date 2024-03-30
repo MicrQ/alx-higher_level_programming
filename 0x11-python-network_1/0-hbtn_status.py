ca6d32ce68bb1d0acafbd29f4895aca7d391733a
MicrQ <mail.micrq@proton.me>
2024-03-30 15:32:26 +0300

    MODIFIED: a python script that fetches 'https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,7 @@ from urllib import request
 
 with request.urlopen('https://alx-intranet.hbtn.io/status') as res:
     body = res.read()
-    print("Body response:\n\
-    \t- type: {}\n\
-    \t- content: {}\n\
-    \t- utf8 content: {}".format(
-        type(body), body, body.decode('utf-8')
-    ))
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
