c5b6e22ed7fa103ca6e2d58830553526b84e7ef2
MicrQ <mail.micrq@proton.me>
2024-03-30 15:50:46 +0300

    MODIFIED: a python script that takes url argument and displays the 'X-Request-Id' of response.

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,10 +2,10 @@
 """ a python script that takes url argument and displays the 'X-Request-Id'
     of response.
 """
+if __name__ == '__main__':
+    from urllib import request
+    from sys import argv
+    """ Importing necessary modules """
 
-from urllib import request
-from sys import argv
-""" Importing necessary modules """
-
-with request.urlopen(argv[1]) as res:
-    print(res.getheader('X-Request-Id'))
+    with request.urlopen(argv[1]) as res:
+        print(res.getheader('X-Request-Id'))
