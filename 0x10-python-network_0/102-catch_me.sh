d7d5dcee04daaefafe328e04b44e5c72c08d3869
MicrQ <mail.micrq@proton.me>
2024-03-31 09:51:41 +0300

    task 102... modified

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that causes the server to respond 'You got me!'
-curl -s "0.0.0.0:5000/catch_me?data=You got me!"
+curl -sLX PUT -H "origin: You got me!" 0.0.0.0:5000/catch_me
