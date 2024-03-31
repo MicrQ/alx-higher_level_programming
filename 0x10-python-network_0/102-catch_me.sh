bb35ea137b627b357a715b2c346ba7c3796d944b
MicrQ <mail.micrq@proton.me>
2024-03-31 10:09:42 +0300

    user id added

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that causes the server to respond 'You got me!'
-curl -sLX PUT -H "origin: You got me!" 0.0.0.0:5000/catch_me
+curl -sLX PUT -H "origin: You got me!" -d "user_id=98" 0.0.0.0:5000/catch_me
