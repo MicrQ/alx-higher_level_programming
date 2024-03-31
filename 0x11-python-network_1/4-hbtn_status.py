e29d2dd5be3dfbe507ab9c9932a4774bc3d8cde0
MicrQ <mail.micrq@proton.me>
2024-03-31 12:13:39 +0300

    a script that fetches 'https://alx-intranet.hbtn.io/status' using requests module

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" a script that fetches https://alx-intranet.hbtn.io/status """
+
+if __name__ == '__main__':
+    import requests
+    """ module to fetch the page """
+
+    res = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t - {}".format(type(res.text)))
+    print("\t - {}".format(res.text))
