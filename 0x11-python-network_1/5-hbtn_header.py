ad3b5de78e1c0eb66daf17da7aab7eb39079c402
MicrQ <mail.micrq@proton.me>
2024-03-31 14:15:04 +0300

    a script that sends a get request and prints the value of X-Request-Id

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" a script that sends a GET request and
+    prints the value of X-Request-Id header.
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    """ necessary packages """
+
+    res = requests.get(argv[1])
+    print(res.headers['X-Request-Id'])
