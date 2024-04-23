a94f91455b979898bdfd642a719c17f79eafefe4
MicrQ <mail.micrq@proton.me>
2024-04-24 01:20:35 +0300

    a js script that prints the number of movies where the character “Wedge Antilles” is present. the id of "Wedge Antilles" is 18

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+
+const options = {
+  url: process.argv[2],
+  json: true
+};
+
+let count = 0;
+
+request(options, (error, response) => {
+  if (error) throw error;
+  response.body.results.forEach(film => {
+    if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+      count++;
+    }
+  });
+  console.log(count);
+});
