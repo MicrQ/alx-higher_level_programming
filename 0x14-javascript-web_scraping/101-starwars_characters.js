dc1862bc601c738bbf3cff90f9446cdec2e275ab
MicrQ <mail.micrq@proton.me>
2024-04-24 17:29:10 +0300

    modified to use JSON.parse method

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-const options = {
-  url: 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2],
-  json: true
-};
-request(options, (error, response, body) => {
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request(url, (error, response, body) => {
   if (!error) {
-    for (let i = 0; i < body.characters.length; i++) {
-      request({ url: body.characters[i], json: true }, (err, res, bd) => {
+    const jsonResponse = JSON.parse(body).characters;
+    for (let i = 0; i < jsonResponse.length; i++) {
+      request({ url: jsonResponse[i], json: true }, (err, res, bd) => {
         if (!err) {
           console.log(bd.name);
         }
